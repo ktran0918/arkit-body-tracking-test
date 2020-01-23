2d3047e2f5c96026f406b80b76ffa0577d9b9bdb
Khoa Tran <k.tran.0918@outlook.com>
2020-01-22 23:55:07 -0800

    Initial Commit

---
+++ ARKit test 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKit test 3
+//
+//  Created by Khoa Tran on 1/22/20.
+//  Copyright © 2020 Khoa Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
